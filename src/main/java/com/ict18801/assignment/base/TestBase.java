527d65b19fefb69794ad7e131117dfe75fae1db7
Ahamed Aathil <ahamedaathil.5@gmail.com>
2023-02-20 22:21:04 +0530

    LoginPage Done

---
+++ src/main/java/com/ict18801/assignment/base/TestBase.java
@@ -0,0 +1,46 @@
+package com.ict18801.assignment.base;
+
+import com.ict18801.assignment.util.TestUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public static WebDriver driver;
+    public static Properties prop;
+
+    public TestBase() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream("E:\\STUDY MATERIAL SEM 8\\Emerging Technology\\Assignments\\1\\ICT-18-801\\src\\main\\java\\com\\ict18801\\assignment\\config\\config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initialization(){
+        String browserName = prop.getProperty("browser");
+        if (browserName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver","E:\\ChromeDrivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browserName.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver","E:\\ChromeDrivers\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtil.PAGE_LOAD_TIMEOUT));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.IMPLICIT_WAIT));
+        driver.get(prop.getProperty("url"));
+    }
+}
---
+++ src/main/java/com/ict18801/assignment/pages/HomePage.java
@@ -0,0 +1,7 @@
+package com.ict18801.assignment.pages;
+
+import com.ict18801.assignment.base.TestBase;
+
+public class HomePage extends TestBase {
+
+}
