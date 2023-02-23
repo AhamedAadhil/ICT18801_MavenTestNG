06b2194c07c23909bf187dcbac24912d263e8e02
Ahamed Aathil <ahamedaathil.5@gmail.com>
2023-02-23 11:03:49 +0530

    Data Drivern Test and Screenshot done
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- src/main/java/com/ict18801/assignment/base/TestBase.java
+++ src/main/java/com/ict18801/assignment/base/TestBase.java
@@ -1,10 +1,14 @@
 package com.ict18801.assignment.base;
 
 import com.ict18801.assignment.util.TestUtil;
+import com.ict18801.assignment.util.WebEventListener;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,10 +16,12 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
-public class TestBase {
+public abstract class TestBase {
     public static Logger logger = Logger.getLogger(TestBase.class);
     public static WebDriver driver;
     public static Properties prop;
+    public static EventFiringWebDriver e_driver;
+    public static WebEventListener eventlistner;
 
     public TestBase() {
         try {
@@ -38,10 +44,17 @@ public class TestBase {
             System.setProperty("webdriver.gecko.driver", "E:\\ChromeDrivers\\geckodriver.exe");
             driver = new FirefoxDriver();
         }
+
+        e_driver = new EventFiringWebDriver(driver);
+        eventlistner = new WebEventListener();
+        e_driver.register(eventlistner);
+        driver = e_driver;
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtil.PAGE_LOAD_TIMEOUT));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.IMPLICIT_WAIT));
         driver.get(prop.getProperty("url"));
     }
+
 }
--- src/test/java/com/ict18801/assignment/tests/UsersPageTest.java
+++ src/test/java/com/ict18801/assignment/tests/UsersPageTest.java
@@ -4,15 +4,20 @@ import com.ict18801.assignment.base.TestBase;
 import com.ict18801.assignment.pages.HomePage;
 import com.ict18801.assignment.pages.LoginPage;
 import com.ict18801.assignment.pages.UsersPage;
+import com.ict18801.assignment.util.TestUtil;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class UsersPageTest extends TestBase {
     LoginPage loginPage;
     HomePage homePage;
     UsersPage usersPage;
+    String sheetName = "userData";
 
     public UsersPageTest() {
         super();
@@ -38,9 +43,16 @@ public class UsersPageTest extends TestBase {
         Assert.assertTrue(isPannelVisible);
     }
 
-    @Test(priority = 3)
-    public void fillTheFormTest() {
-        usersPage.fillTheForm("Ahamed", "Aathil", "A", "aahamed@gmail.com");
+    @DataProvider
+    public Object[][] getDataFromSheet() {
+        Object[][] data = TestUtil.getTestData(sheetName);
+        return data;
+    }
+
+    @Test(priority = 3, dataProvider = "getDataFromSheet")
+    public void fillTheFormTest(String firstname, String middlename, String lastname, String email) {
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.PAGE_LOAD_TIMEOUT));
+        usersPage.fillTheForm(firstname, lastname, middlename, email);
     }
 
     @AfterMethod
