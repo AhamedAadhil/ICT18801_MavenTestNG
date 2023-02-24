3d1e671d6f198f30fe9e26013716ca9f221e526b
Ahamed Aathil <ahamedaathil.5@gmail.com>
2023-02-24 10:50:47 +0530

    log4j properties added
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- src/main/java/com/ict18801/assignment/pages/UsersPage.java
+++ src/main/java/com/ict18801/assignment/pages/UsersPage.java
@@ -2,15 +2,19 @@ package com.ict18801.assignment.pages;
 
 import com.ict18801.assignment.base.TestBase;
 import com.ict18801.assignment.util.TestUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 
 public class UsersPage extends TestBase {
+
+    org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(UsersPage.class);
     @FindBy(xpath = "//div[contains(text(),'Users')]")
     WebElement usersText;
     @FindBy(xpath = "//div[contains(text(),'New User')]")
@@ -47,11 +51,22 @@ public class UsersPage extends TestBase {
 
     public void fillTheForm(String fname, String lname, String mname, String mail) {
         clickNewUserButton();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.IMPLICIT_WAIT));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TestUtil.PAGE_LOAD_TIMEOUT));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='First Name' and @id='createUserPanel_firstNameField']")));
         firstname.sendKeys(fname);
+        log.info(firstname);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='MI' and @id='createUserPanel_middleNameField']")));
         middlename.sendKeys(mname);
+        log.info(middlename);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Last Name' and @id='createUserPanel_lastNameField']")));
         lastname.sendKeys(lname);
+        log.info(lastname);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Email' and @id='createUserPanel_emailField']")));
         email.sendKeys(mail);
+        log.info(email);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Save & Send Invitation')]")));
         saveButton.click();
+        log.info(saveButton);
     }
 }
